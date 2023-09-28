b50a142d7df2ac05b265f7ab034f2f7a0107e538
samuel <sammykuria300@gmail.com>
2023-09-28 09:25:33 +0300

    Quick commit

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution here
+const drivers = ["bobby", "sammy", "sally", "annette", "sarah", "Bobby", "Bobby"];
+
+function findMatching(array, searchName) {
+  const searchNameLower = searchName.toLowerCase();
+
+  return array.filter(function (element) {
+    const elementLower = element.toLowerCase();
+    return elementLower === searchNameLower;
+  });
+}
+
+console.log(findMatching(drivers, "bobby"))
+
+function fuzzyMatch(drivers, query) {
+    const queryLower = query.toLowerCase();
+  
+    return drivers.filter(function (driver) {
+      const driverLower = driver.toLowerCase();
+      return driverLower.startsWith(queryLower);
+    });
+  }
+   
+  function matchName(drivers, name) {
+    return drivers.filter(function (driver) {
+      return driver.name === name;
+    });
+  }
+   
+  
+  
